ffc0e0bd23a1adf12248cd54c70dfe1b10fd63a1
Deeveish <jvdeeveish@gmail.com>
2021-12-16 12:29:22 +0530

    Check branch

--- src/main/java/com/dev/UserServlet/LoginServlet.java
+++ src/main/java/com/dev/UserServlet/LoginServlet.java
@@ -48,6 +48,7 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);// command for checking for git hub purpose
+		//brach command to chck in git
 	}
 
 }
